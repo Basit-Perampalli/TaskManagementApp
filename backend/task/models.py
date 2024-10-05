6b3896ff12caeaf0db5265bc625bdeb9735f6cce
Atharva-5 <atharvajoshi664@gmail.com>
2024-10-05 23:27:55 +0530

    Backend part
    Model created, database created.

--- backend/task/models.py
+++ backend/task/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High'),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    status = models.BooleanField(default=False)  # False = Not Completed, True = Completed
+    due_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
