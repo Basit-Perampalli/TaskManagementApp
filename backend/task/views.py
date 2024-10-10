ed0adfb053556d3ee9676a6ee87cdccd613fb0b1
Atharva-5 <atharvajoshi664@gmail.com>
2024-10-10 22:44:20 +0530

    Changed views file of task app

--- backend/task/views.py
+++ backend/task/views.py
@@ -1,16 +1,69 @@
-from rest_framework import generics
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Task
 from .serializers import TaskSerializer
 
 
-class TaskListCreateView(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-    permission_classes = [IsAuthenticated]  # Adding IsAuthenticated permission
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserTasks(request):
+    user = request.user
+    user_tasks = Task.objects.filter(author=user)
+    serializer = TaskSerializer(user_tasks, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-    permission_classes = [IsAuthenticated]  # Adding IsAuthenticated permission
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getAllTasks(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createTask(request):
+    data = request.data
+    serializer = TaskSerializer(data=data)
+
+    if serializer.is_valid():
+        # Set the author to the current user
+        serializer.save(author=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateTask(request, task_id):
+    try:
+        task = Task.objects.get(id=task_id)
+    except Task.DoesNotExist:
+        return Response({'error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if task.author != request.user:
+        return Response({'error': 'You do not have permission to edit this task'}, status=status.HTTP_403_FORBIDDEN)
+
+    serializer = TaskSerializer(task, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def deleteTask(request, task_id):
+    try:
+        task = Task.objects.get(id=task_id)
+    except Task.DoesNotExist:
+        return Response({'error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if task.author != request.user:
+        return Response({'error': 'You do not have permission to delete this task'}, status=status.HTTP_403_FORBIDDEN)
+
+    task.delete()
+    return Response({'message': 'Task deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
